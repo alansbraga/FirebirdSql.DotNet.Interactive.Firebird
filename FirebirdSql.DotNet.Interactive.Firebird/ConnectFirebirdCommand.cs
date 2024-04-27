aebf77556acdff3b8ab33ec472674cfe91f7a47f
Alan Braga <alansbraga@gmail.com>
2024-04-27 11:09:41 -0300

    Working version. No variable sharing.

---
+++ FirebirdSql.DotNet.Interactive.Firebird/ConnectFirebirdCommand.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Connection;
+
+namespace FirebirdSql.DotNet.Interactive.Firebird;
+
+public class ConnectFirebirdCommand : ConnectKernelCommand
+{
+    public ConnectFirebirdCommand()
+        : base("firebird", "Connects to a Firebird database")
+    {
+        Add(ConnectionStringArgument);
+    }
+
+    public Argument<string> ConnectionStringArgument { get; } =
+        new("connectionString", "The connection string used to connect to the database");
+
+    public override Task<IEnumerable<Kernel>> ConnectKernelsAsync(
+        KernelInvocationContext context,
+        InvocationContext commandLineContext)
+    {
+        var connectionString = commandLineContext.ParseResult.GetValueForArgument(ConnectionStringArgument);
+        var localName = commandLineContext.ParseResult.GetValueForOption(KernelNameOption);
+        var kernel = new FirebirdKernel($"sql-{localName}", connectionString);
+        return Task.FromResult<IEnumerable<Kernel>>(new[] { kernel });
+    }
+}
